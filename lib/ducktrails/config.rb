0b51fe07ef1439a86b7b8423478897e328e91f2a
Kevin Brown <chevinbrown@gmail.com>
2017-08-04 14:18:52 -0500

    Config cleanup

--- lib/ducktrails/config.rb
+++ lib/ducktrails/config.rb
@@ -1,46 +1,21 @@
 require 'active_support/configurable'
 
 module Ducktrails
-  # Configures global settings for Ducktrails
-  #   Ducktrails.configure do |config|
-  #     config.default_per_page = 10
-  #   end
   def self.configure(&block)
     yield @config ||= Ducktrails::Configuration.new
   end
 
-  # Global settings for Ducktrails
   def self.config
-    @config
+    @config ||= self.config
   end
 
-  class Configuration #:nodoc:
+  class Configuration
     include ActiveSupport::Configurable
-    config_accessor :root_path
-    config_accessor :theme
-    config_accessor :home_name
-    # TODO: How to set this?
-    config_accessor :collection_prefix
-    config_accessor :default_key
-    config_accessor :fallback_to_uri
-    config
-    def param_name
-      config.param_name.respond_to?(:call) ? config.param_name.call : config.param_name
-    end
 
-    # define param_name writer (copied from AS::Configurable)
-    writer, line = 'def param_name=(value); config.param_name = value; end', __LINE__
-    singleton_class.class_eval writer, __FILE__, line
-    class_eval writer, __FILE__, line
-  end
-
-  # this is ugly. why can't we pass the default value to config_accessor...?
-  configure do |config|
-    config.root_path = '/'
-    config.theme = 'ducktrails'
-    config.home_name = 'Home'
-    config.collection_prefix = 'All'
-    config.default_key = :id
-    config.fallback_to_uri = true
+    config_accessor :theme { 'ducktrails' }
+    config_accessor :home_name { 'Home' }
+    config_accessor :root_path { '/' }
+    config_accessor :collection_prefix { 'All' }
+    config_accessor :default_key { :id }
   end
 end
--- lib/ducktrails/view_helpers.rb
+++ lib/ducktrails/view_helpers.rb
@@ -15,20 +15,11 @@ module Ducktrails
       ducktrail_render(LinkCollection.new(resources, current_uri).links)
     end
 
-    # TODO: Take this out and just use a view?
-    def home_crumb(options = {})
-      link_to(duck_config.home_name, duck_config.root_path)
-    end
-
     def build_resource(&block)
       block_given? ? yield : nil
     end
 
     private
-
-    def duck_config
-      @config ||= Ducktrails.config
-    end
 
     def current_uri
       request.fullpath
