c7bfedac430a0f48dbfaa0fc71611ef1968d57e4
Kevin Brown <chevinbrown@gmail.com>
2018-02-16 15:37:30 -0600

    Base out test--capybara will come later

--- test/ducktrails_test.rb
+++ test/ducktrails_test.rb
@@ -1,24 +1,7 @@
 require 'test_helper'
 
-class Ducktrails::Test < ActionDispatch::IntegrationTest
-  include Capybara::DSL
-
+class Ducktrails::Test < ActiveSupport::TestCase
   test 'ducktrails exists' do
     assert_kind_of Module, Ducktrails
-  end
-
-  # TODO: Complete full testing
-  # 1. Test CRUD with no breadcrumbs block given
-  # 2. Test CRUD with some resources given
-  # 3. Test config options
-  test 'render breadcrumb' do
-    visit '/'
-    assert page.has_content?('Home')
-  end
-
-  test 'render collection bredcrumb' do
-    visit posts_path
-    assert page.has_content?('Home')
-    assert page.has_content?('All Posts')
   end
 end
