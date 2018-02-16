65d8c7925064a9a0c1168116cabe119361173557
Kevin Brown <chevinbrown@gmail.com>
2018-02-16 15:36:10 -0600

    Switch to sqlite

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,9 +7,9 @@ require "ducktrails"
 
 module Dummy
   class Application < Rails::Application
-    FactoryGirl.definition_file_paths << Pathname.new("../factories")
-    FactoryGirl.definition_file_paths.uniq!
-    FactoryGirl.find_definitions
+    # FactoryGirl.definition_file_paths << Pathname.new("../factories")
+    # FactoryGirl.definition_file_paths.uniq!
+    # FactoryGirl.find_definitions
     config.eager_load = false
   end
 end
